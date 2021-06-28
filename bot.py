57084343af231344eb3e5b9284a4facfc15b1f7e
KirillProsvirin <kirill.prosvirin@gmail.com>
2021-06-28 21:50:15 +0300

    Final version

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-from style_transfer import * 
 from aiogram import Bot, types
 from aiogram.utils import executor
 from aiogram.utils.emoji import emojize
@@ -8,7 +7,7 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.types.message import ContentType
 from aiogram.utils.markdown import text, bold, italic, code, pre
 from aiogram.types import ParseMode, InputMediaPhoto, InputMediaVideo, ChatActions, InputFile
-
+from style_transfer import * 
 from config import TOKEN
 
 logging.basicConfig(format=u'%(filename)s [ LINE:%(lineno)+3s ]#%(levelname)+8s [%(asctime)s]  %(message)s',
@@ -23,42 +22,43 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.reply('Привет!\nИспользуй /help, '
-                        'чтобы узнать список доступных команд!')
+                        'чтобы узнать, что я умею.')
 
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message): 
     msg = text('Я могу сделать одну фотографии в стиле другой! Для этого нужно прислать мне две фотографии:',  
-        'Первая -- это та, у которой будет изменен стиль,', 'Вторая -- это та, в каком стиле все будет.', sep='\n')
+        'Первая -- это та, у которой будет изменен стиль,', 'Вторая -- это та, в каком стиле все будет.', 
+        '', 'Чтобы было красиво, фотографию со стилем бери не больше чем 256х256', sep='\n')
     await message.reply(msg, parse_mode=ParseMode.MARKDOWN)
 
-    
+
 @dp.message_handler(content_types=['photo'])
 async def process_photo_command(message: types.Message):
     global one_photo_flg, cnn, cnn_normalization_mean, cnn_normalization_std
 
-    path1 = 'content/'+ str(message.from_user.id) + '.png'
-    path2 = 'style/'+ str(message.from_user.id) + '.png'
-    path3 = str(message.from_user.id) + '.png'
+    content_path = 'content/'+ str(message.from_user.id) + '.png'
+    style_path = 'style/'+ str(message.from_user.id) + '.png'
+    gen_path = str(message.from_user.id) + '.png'
 
     if one_photo_flg == 1:
         one_photo_flg = 0
-        await message.photo[-1].download(path1)
+        await message.photo[-1].download(content_path)
         message_text = 'Вот это фоточка!\nСохранил ее. Теперь скидывай вторую.'
         await bot.send_message(message.from_user.id, message_text)
         return
 
     one_photo_flg = 1
-    await message.photo[-1].download(path2)
+    await message.photo[-1].download(style_path)
 
-    content_img = image_loader(path1)
-    style_img = image_loader(path2)
+    content_img = image_loader(content_path)
+    style_img = image_loader(style_path)
 
     await bot.send_chat_action(message.from_user.id, ChatActions.UPLOAD_DOCUMENT)
-    make_magic(cnn, cnn_normalization_mean,cnn_normalization_std, content_img, style_img, path3)
+    make_magic(cnn, cnn_normalization_mean,cnn_normalization_std, content_img, style_img, gen_path)
 
-    message_text = 'Вот это фоточка!\nСохранил ее. Теперь скидывай вторую.'
-    await bot.send_photo(message.from_user.id, InputFile('generated/' + path3))
+    message_text = 'Вот это фоточка!\nСохранил ее. Теперь скидывай вторую со стилем.'
+    await bot.send_photo(message.from_user.id, InputFile('generated/' + gen_path))
 
 
 @dp.message_handler()
--- style_transfer.py
+++ style_transfer.py
@@ -1,6 +1,4 @@
 from PIL import Image
-
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -8,10 +6,8 @@ import torch.optim as optim
 import os
 import matplotlib.pyplot as plt
 from torchvision.utils import save_image
-
 import torchvision.transforms as transforms
 import torchvision.models as models
-
 import copy
 
 imsize = 200
@@ -27,17 +23,13 @@ def image_loader(image_name):
     image = loader(image).unsqueeze(0)
     return image.to(device, torch.float)
 
-
-# style_img = image_loader("style/picasso.jpg")# as well as here
-# content_img = image_loader("content/dancing.jpg")#измените путь на тот который у вас.
-
 unloader = transforms.ToPILImage() # тензор в кратинку  
 
 plt.ion() 
 
 def imshow(tensor, title=None):
     image = tensor.cpu().clone()   
-    image = image.squeeze(0)      # функция для отрисовки изображения
+    image = image.squeeze(0)
     image = unloader(image)
     plt.imshow(image)
     if title is not None:
@@ -48,35 +40,22 @@ class ContentLoss(nn.Module):
 
         def __init__(self, target,):
             super(ContentLoss, self).__init__()
-            # we 'detach' the target content from the tree used
-            # to dynamically compute the gradient: this is a stated value,
-            # not a variable. Otherwise the forward method of the criterion
-            # will throw an error.
-            self.target = target.detach() # это константа. Убираем ее из дерева вычеслений
-            # self.loss = F.mse_loss(self.target, self.target) # to initialize with something
+            self.target = target.detach() 
 
         def forward(self, input):
             self.loss = F.mse_loss(input, self.target)
             return input
 
 def gram_matrix(input):
-        batch_size , h, w, f_map_num = input.size()  # batch size(=1)
-        # b=number of feature maps
-        # (h,w)=dimensions of a feature map (N=h*w)
-
-        features = input.view(batch_size * h, w * f_map_num)  # resise F_XL into \hat F_XL
-
-        G = torch.mm(features, features.t())  # compute the gram product
-
-        # we 'normalize' the values of the gram matrix
-        # by dividing by the number of element in each feature maps.
+        batch_size , h, w, f_map_num = input.size()
+        features = input.view(batch_size * h, w * f_map_num)
+        G = torch.mm(features, features.t())
         return G.div(batch_size * h * w * f_map_num)
 
 class StyleLoss(nn.Module):
         def __init__(self, target_feature):
             super(StyleLoss, self).__init__()
             self.target = gram_matrix(target_feature).detach()
-            # self.loss = F.mse_loss(self.target, self.target)# to initialize with something
 
         def forward(self, input):
             G = gram_matrix(input)
@@ -89,9 +68,6 @@ cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
 class Normalization(nn.Module):
         def __init__(self, mean, std):
             super(Normalization, self).__init__()
-            # .view the mean and std to make them [C x 1 x 1] so that they can
-            # directly work with image Tensor of shape [B x C x H x W].
-            # B is batch size. C is number of channels. H is height and W is width.
             self.mean = torch.tensor(mean).view(-1, 1, 1)
             self.std = torch.tensor(std).view(-1, 1, 1)
 
@@ -113,8 +89,6 @@ def get_style_model_and_losses(cnn, normalization_mean, normalization_std,
         # normalization module
         normalization = Normalization(normalization_mean, normalization_std).to(device)
 
-        # just in order to have an iterable access to or list of content/syle
-        # losses
         content_losses = []
         style_losses = []
 
@@ -129,9 +103,6 @@ def get_style_model_and_losses(cnn, normalization_mean, normalization_std,
                 name = 'conv_{}'.format(i)
             elif isinstance(layer, nn.ReLU):
                 name = 'relu_{}'.format(i)
-                # The in-place version doesn't play very nicely with the ContentLoss
-                # and StyleLoss we insert below. So we replace with out-of-place
-                # ones here.
                 #Переопределим relu уровень
                 layer = nn.ReLU(inplace=False)
             elif isinstance(layer, nn.MaxPool2d):
@@ -157,7 +128,6 @@ def get_style_model_and_losses(cnn, normalization_mean, normalization_std,
                 model.add_module("style_loss_{}".format(i), style_loss)
                 style_losses.append(style_loss)
 
-        # now we trim off the layers after the last content and style losses
         #выбрасываем все уровни после последенего styel loss или content loss
         for i in range(len(model) - 1, -1, -1):
             if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
@@ -168,8 +138,6 @@ def get_style_model_and_losses(cnn, normalization_mean, normalization_std,
         return model, style_losses, content_losses
 
 def get_input_optimizer(input_img):
-        # this line to show that input is a parameter that requires a gradient
-        #добоваляет содержимое тензора катринки в список изменяемых оптимизатором параметров
         optimizer = optim.LBFGS([input_img.requires_grad_()]) 
         return optimizer
 
@@ -181,18 +149,12 @@ def run_style_transfer(cnn, normalization_mean, normalization_std,
         model, style_losses, content_losses = get_style_model_and_losses(cnn,
             normalization_mean, normalization_std, style_img, content_img)
         optimizer = get_input_optimizer(input_img)
-
         print('Optimizing..')
         run = [0]
-        previous_loss = 1e6
-        flg_ex = 0
 
-        while run[0] <= num_steps:# and flg_ex == 0:
+        while run[0] <= num_steps:
 
             def closure():
-                # correct the values 
-                # это для того, чтобы значения тензора картинки не выходили за пределы [0;1]
-                nonlocal previous_loss, flg_ex
                 input_img.data.clamp_(0, 1)
 
                 optimizer.zero_grad()
@@ -207,7 +169,7 @@ def run_style_transfer(cnn, normalization_mean, normalization_std,
                 for cl in content_losses:
                     content_score += cl.loss
                 
-                #взвешивание ощибки
+                #взвешивание ошибки
                 style_score *= style_weight
                 content_score *= content_weight
 
@@ -220,10 +182,6 @@ def run_style_transfer(cnn, normalization_mean, normalization_std,
                     print('Style Loss : {:4f} Content Loss: {:4f}'.format(
                         style_score.item(), content_score.item()))
                     print()
-
-                if (previous_loss - loss.item()) < 1e-6:
-                    flg_ex = 1
-                previous_loss = loss
 
                 return style_score + content_score
 
@@ -245,9 +203,3 @@ def make_magic(cnn, cnn_normalization_mean,cnn_normalization_std, content_img, s
     os.makedirs(sample_dir, exist_ok=True)
     output = output.reshape(3, imsize, imsize)
     save_image(output, os.path.join(sample_dir, path))
-
-
-
-
-
-
